0e72e926db4c591eb2bd19738cef8a71cdacb1c7
Yueting Jia <psxyj11@nottingham.ac.uk>
2026-05-09 19:16:36 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -54,8 +54,8 @@ var editable = true;
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
-    id: 'Beck2016Visual'
+   html: 'Green AI Interactions: A Literature Review of Sustainable Design, Human-Computer Interaction, and User-Centered Approaches',
+    id: ''
 };
 
 /**
